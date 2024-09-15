18198036fa18e7e6ddb532a6a3084000471eab2b
hhow09 <hhow09@gmail.com>
2024-09-15 15:41:29 +0800

    feat: pass  Parse Correlation ID #wa6 - https://app.codecrafters.io/courses/kafka/stages/wa6 - parse request header

--- app/server.go
+++ app/server.go
@@ -8,16 +8,9 @@ import (
 	"github.com/codecrafters-io/redis-starter-go/app/wireprotocol"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
@@ -35,12 +28,10 @@ func main() {
 
 func handleConn(conn net.Conn) {
 	defer conn.Close()
-	// In this stage, you're expected to hardcode the correlation ID as 7 in your response.
-	// We'll get to reading this value from the request in later stages.
-	_, err := conn.Read(make([]byte, 1024))
+	header, err := wireprotocol.ReadRequestHeaderV2(conn)
 	if err != nil {
 		fmt.Println("Error reading from connection: ", err.Error())
 		os.Exit(1)
 	}
-	conn.Write(wireprotocol.ResponseHeaderV0(7))
+	conn.Write(wireprotocol.ResponseHeaderV0(header.CorrelationID))
 }
--- app/wireprotocol/wire.go
+++ app/wireprotocol/wire.go
@@ -1,6 +1,9 @@
 package wireprotocol
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+	"io"
+)
 
 // ResponseHeaderV0 sends a response header
 // first 4 bytes: response size
@@ -11,4 +14,33 @@ func ResponseHeaderV0(correlationID uint32) []byte {
 	binary.BigEndian.PutUint32(b[:4], uint32(len))
 	binary.BigEndian.PutUint32(b[4:], correlationID)
 	return b
+}
+
+type HeaderV2 struct {
+	RequestApiKey uint16
+	// The version of the API for the request
+	RequestApiVersion uint16
+	// A unique identifier for the request
+	CorrelationID uint32
+}
+
+func ReadRequestHeaderV2(r io.Reader) (HeaderV2, error) {
+	buf := make([]byte, 1024)
+	_, err := r.Read(buf)
+	if err != nil {
+		return HeaderV2{}, err
+	}
+	msgLen := binary.BigEndian.Uint32(buf[:4])
+	if msgLen > 1024 {
+		return HeaderV2{}, io.ErrShortBuffer
+	}
+	requestApiKey := binary.BigEndian.Uint16(buf[4:6])
+	requestApiVersion := binary.BigEndian.Uint16(buf[6:8])
+	correlationID := binary.BigEndian.Uint32(buf[8:12])
+	// TODO other fields
+	return HeaderV2{
+		RequestApiKey:     requestApiKey,
+		RequestApiVersion: requestApiVersion,
+		CorrelationID:     correlationID,
+	}, nil
 }
